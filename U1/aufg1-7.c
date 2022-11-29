d43211d2f62a2233d9b2d156fb8676175630960e
tara-kh <khorramnia.tara@gmail.com>
2022-11-29 14:15:46 +0100

    challenge solved

--- U1/aufg1-7.c
+++ U1/aufg1-7.c
@@ -1,20 +1,16 @@
 /* Datei aufg1-7.c */
-/* Umrechnung von Winkelmaß in Bogenmaß *
+/* Umrechnung von Winkelmaï¿½ in Bogenmaï¿½ */
 
 #include <stdlib.h>
-#include <stdio>
+#include <stdio.h>
 
-int main(void);
+int main(void)
 {
-	double winkel bogen;
-	dOuble faktor=2*3,141592654/360
-	printf("Geben Sie den Winkel im Winkelmass ein: ");
-	scanf("%f", winkel);
-	bogen := winkel*Faktor;
-	printf("\nEin Winkel von%dGrad entspricht %f rad\n\n,winkel.bogen);
+	double winkel, bogen;
+	double faktor = (2 * 3.141592654) / 360;
+	printf("Geben Sie den Winkel im Winkelmass ein:\n ");
+	scanf("%lf", &winkel);
+	bogen = winkel * faktor;
+	printf("\nEin Winkel von %f Grad entspricht %f rad\n\n", winkel, bogen);
 	system("Pause");
 }
-}
-
-
-
