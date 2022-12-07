5f998892c1a72c8f28489322eb11d5cc34bda5ba
tara-kh <khorramnia.tara@gmail.com>
2022-12-07 22:52:21 +0100

    Comment added

---
+++ U3/Aufg4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+
+    double x, f = 1;
+    double Summand = 1;
+    int i = 1;
+    double Mul = 1;
+    int Operator = 1;
+    double lnX = 0;
+
+    do
+    {
+        printf("0 < x <= 2 eingeben:\n");
+        scanf("%lf", &x);
+
+    } while (x < 0 || x > 2);
+
+    for (i = 1; lnX <= 0.00001; i++)
+    {
+
+        f = f * (x - 1);
+        Summand = (1 / i) * f * Operator;
+        lnX = lnX + Summand;
+        Operator = -Operator;
+        printf("%f\n", lnX);
+    }
+    printf("%lf", lnX);
+
+    return 0;
+}
