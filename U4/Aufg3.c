7f4b412d8a3d47af13cb7e67fc8cca7a513eb1e2
tara-kh <khorramnia.tara@gmail.com>
2022-12-29 18:06:45 +0100

    challenge solved

--- U4/Aufg3.c
+++ U4/Aufg3.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-int main()
+int funk(char *str)
 {
-
-    char str[50];
     int counter = 0;
-    printf("Geben sie ein ganze Zahl ein : \n");
-    scanf("%s", str);
-    printf("%s\n", str);
-
     int i = 0;
     while (str[i] != '\0')
     {
@@ -19,6 +13,42 @@ int main()
 
         i++;
     }
-    printf("c = %d\n", counter);
+    return counter;
+}
+
+int main()
+{
+    char str[50];
+    int stop = 0;
+    int result;
+    do
+    {
+
+        printf("Geben sie ein ganze Zahl ein : \n");
+        scanf("%s", str);
+
+        result = funk(str);
+        printf("c = %d\n", result);
+
+        printf("Wiederholen?  1-ja  0-nein\n");
+        int wiederholen;
+        scanf("%d", &wiederholen);
+        if (wiederholen == 0)
+        {
+            break;
+        }
+    } while (1);
+
     return 0;
 }
+
+/*
+1- baraye ferestadane string be function ay * pointer estefade mikonim
+
+2- function  ma az noe int ast chon mikhahim meghdar counter ra bargardanad va be result berizad.
+
+3- do while baes mishavad ke code avalain bar dae har sharayeti ejra shavad
+
+4- while(1) baes mishavad ke ma yek loope binahayat dashte bashim va ba break an ra motevaghef konim.
+
+5- wiederholen == 0 barabar ast ba !wiederholen
