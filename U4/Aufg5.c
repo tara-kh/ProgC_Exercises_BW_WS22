bf770252744e71f047cb0a60eb4393340d3ee650
tara-kh <khorramnia.tara@gmail.com>
2022-12-30 13:06:41 +0100

    challenge solved

---
+++ U4/Aufg5.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <math.h>
+
+double funk1(double zahl, double potentz)
+{
+    int i = 1;
+    double mul = 1;
+    while (i <= potentz)
+    {
+        mul = mul * zahl;
+        i++;
+    }
+    return mul;
+}
+double funk2(double zahl, double potentz)
+{
+
+    if (potentz == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        double result2 = zahl * pow(zahl, potentz - 1);
+        return result2;
+    }
+}
+
+int main()
+{
+
+    double Zahl;
+    int Potentz = 0;
+
+    printf("Geben Sie die ganze Zahl ein: ");
+    scanf("%lf", &Zahl);
+    do
+    {
+        if (Potentz < 0)
+        {
+            printf("Potenz weniger als 0, ist nicht erlaubt!!\n");
+        }
+        printf("Geben Sie die Potentz Zahl ein: ");
+        scanf("%d", &Potentz);
+    } while (Potentz < 0);
+
+    double result1 = funk1(Zahl, Potentz);
+    double result2 = funk2(Zahl, Potentz);
+
+    printf("Result1 = %lf\n", result1);
+    printf("Result2 = %lf\n", result2);
+
+    return 0;
+}
