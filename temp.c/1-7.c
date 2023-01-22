6a8754b17779ef73ca02cede245f88f0adc9b581
tara-kh <khorramnia.tara@gmail.com>
2023-01-22 14:45:58 +0100

    .

---
+++ temp.c/1-7.c
@@ -0,0 +1,14 @@
+/* Datei aufg1-7.c */
+/* Umrechnung von Winkelmaß in Bogenmaß */
+#include <stdlib.h>
+#include <stdio.h>
+int main(void)
+{
+    double winkel, bogen;
+    double faktor = (2 * 3.141592654) / 360;
+    printf("Geben Sie den Winkel im Winkelmass ein:\n ");
+    scanf("%lf", &winkel);
+    bogen = winkel * faktor;
+    printf("\nEin Winkel von %f Grad entspricht %f rad\n\n", winkel, bogen);
+    system("Pause");
+}
