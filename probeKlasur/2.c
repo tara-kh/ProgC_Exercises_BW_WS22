c7c754ffdaaea923c340db3738f122b3e6854da4
tara-kh <khorramnia.tara@gmail.com>
2023-02-12 13:03:31 +0100

    .

--- probeKlasur/2.c
+++ probeKlasur/2.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-void replace(char *string)
+int main(void)
 {
+    char string[100];
     int i, j, replacement = 0;
     int length = strlen(string);
+    printf("Enter a string(max 20 charecters, no spaces:)\n");
+    scanf("%s", string);
+    do
+    {
+        for (i = 0; i < length; i++)
+        {
+            if (string[i] == ' ')
+            {
+                printf("keine Leerzeichen!!Geben Sie wieder einen Zeichnenkette ein:\n");
+                scanf("%s", string);
+            }
+        }
+    } while (string[i] != ' ');
 
-    for (i = 0; i < length; i++)
+    if (length > 20)
+    {
+        length = 20;
+    }
+    for (i = 0; i < length - 1; i++)
     {
         if ((string[i] == 'a' || string[i] == 'A') && (string[i + 1] == 'u' || string[i + 1] == 'U'))
         {
@@ -16,13 +34,13 @@ void replace(char *string)
                 {
                     string[i] = 'e';
                     string[i + 1] = 'i';
-                    replacement++;
+                    replacement += 2;
                 }
                 else
                 {
                     string[i] = 'e';
                     string[i + 1] = 'I';
-                    replacement++;
+                    replacement += 2;
                 }
             }
             if (string[i] == 'A')
@@ -31,27 +49,27 @@ void replace(char *string)
                 {
                     string[i] = 'E';
                     string[i + 1] = 'i';
-                    replacement++;
+                    replacement += 2;
                 }
                 else
                 {
                     string[i] = 'E';
                     string[i + 1] = 'I';
-                    replacement++;
+                    replacement += 2;
                 }
             }
         }
     }
-    printf("new string: %s\n", string);
+    if (strlen(string) > 20)
+    {
+        printf("new string: %.20s  (wird abgeschnitten)\n", string);
+    }
+    else
+    {
+        printf("new string: %.20s\n", string);
+    }
+
     printf("How many replacement? %d\n", replacement);
-}
 
-int main(void)
-{
-    char string[21];
-    printf("Enter a string(max 20 charecters, no spaces:)\n");
-    scanf("%s", string);
-
-    replace(string);
     return 0;
 }
