a195e705ff7ec4f61cbe543b873678426bd43362
tara-kh <khorramnia.tara@gmail.com>
2023-02-24 12:07:50 +0100

    .

---
+++ temp.c/axam2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct person
+{
+    char name[30];
+    double weight;
+    double height;
+    double BMI;
+};
+void read_person(struct person *p)
+{
+
+    char esm[30];
+    double vazn;
+    double ghad;
+    printf("name?\n");
+    scanf("%s", esm);
+    printf("Weight?\n");
+    scanf("%lf", &vazn);
+    printf("height\n");
+    scanf("%lf", &ghad);
+
+    p->weight = vazn;
+    p->height = ghad / 100;
+    strcpy(p->name, esm);
+}
+void BMI_calculator(struct person *p)
+{
+
+    p->BMI = (p->weight / (p->height * p->height));
+}
+char *decide(struct person p)
+{
+    char *situ[30];
+    if (p.BMI >= 30)
+    {
+        *situ = "overweight";
+    }
+    else if (p.BMI < 30 && p.BMI >= 25)
+    {
+        *situ = "normal";
+    }
+    else
+    {
+        *situ = "Below";
+    }
+    return *situ;
+}
+int main()
+{
+
+    struct person p1;
+    read_person(&p1);
+    BMI_calculator(&p1);
+    struct person p2;
+    read_person(&p2);
+    BMI_calculator(&p2);
+
+    printf("name: %s\th: %lf\tw: %lf\tbmi: %lf\tsituation: %s\n", p1.name, p1.height, p1.weight, p1.BMI, decide(p1));
+
+    printf("name: %s\th: %lf\tw: %lf\tbmi: %lf\tsituation: %s\n", p2.name, p2.height, p2.weight, p2.BMI, decide(p2));
+
+    return 0;
+}
--- temp.c/exm.c
+++ temp.c/exm.c
@@ -1,118 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-int funk(char string[], char letter)
+struct cylinder
 {
-    int j = 0;
-    int counter = 0;
-    for (j = 0; string[j] != '\0'; j++)
+    double r, h;
+};
+
+int read_cylinder(struct cylinder *c1)
+{
+    double rt, ht;
+    printf("radius?\n");
+    scanf("%lf", &rt);
+    printf("höhe?\n");
+    scanf("%lf", &ht);
+    c1->h = ht;
+    c1->r = rt;
+    if (rt < 0 || ht < 0)
     {
-        if (string[j] == letter)
-        {
-            counter++;
-        }
+        return -1;
     }
-    return counter;
+    return 0;
 }
-//------------------------------------------------------------------------------------------------
-int funk1(char letter)
+double volume_cylinder(struct cylinder *c)
 {
-    int j = 0;
-    if (letter >= '0' && letter <= '9')
+    double v;
+    v = M_PI * (c->r * c->r) * c->h;
+    return v;
+}
+int main()
+{
+    struct cylinder c1;
+    struct cylinder c2;
+
+    int result1 = read_cylinder(&c1);
+
+    int result2 = read_cylinder(&c2);
+
+    double volume1 = volume_cylinder(&c1);
+    double volume2 = volume_cylinder(&c2);
+
+    printf("Radius und Höhe des ersten Zylinders: %.3lf\t%.3lf\n", c1.r, c1.h);
+    printf("Radius und Höhe des zweiten Zylinders: %.3lf\t%.3lf\n", c2.r, c2.h);
+    if (result2 == -1 || result1 == -1)
     {
+        printf("Eingabefehler - Abbruch\n ");
         return 0;
     }
-    else if (letter >= 'A' && letter <= 'Z' || letter >= 'a' && letter <= 'z')
+    printf("zylinder 1: Radius: %.6lf\t, Hoehe %.6lf, Volimen : %.6lf\n", c1.r, c1.h, volume1);
+    printf("zylinder 2: Radius: %.6lf\t, Hoehe %.6lf, Volimen : %.6lf\n", c2.r, c2.h, volume2);
+    if (volume1 == volume2)
     {
-        return 1;
+        printf("Die Volumen der Zylinder sind gleich.\n");
     }
-    else
-    {
-        return 2;
-    }
-}
-//-----------------------------------------------------------------------------------------------
-int ascii(char z)
-{
-    int asc = z;
-    if (asc < 0)
-    {
-        asc = asc + 256;
-    }
-    return asc;
-}
-//-----------------------------------------------------------------------------------------------
-int main(void)
-{
-    char str[] = "Ein Herr faellt vom Stuhl (Erich Kaestner) Es ist bekannt, dass Menschen, die im Sitzen einschlafen, vornueber sinken. Immer tiefer und tiefer. Wenn die Koerpernerven, die trotz des Schlafens munter bleiben, spueren, dass sich das Schwergewicht allzu sehr vom Stuhl entfernt, geben sie dem Kopf einen Ruck. Er fliegt nach rueckwaerts, und das sogenannte Einnicken kann wieder von vorne beginnen. Wenn die Nerven aber den richtigen Augenblick versaeumen, purzelt der Schlaefer vom Stuhl. Alfredo Torres, einem braven Buerger von Buenos Aires, erging es so. Er schlief ein und fiel vom Stuhl. Waere ihm das zu Hause passiert, haette es niemand weiter erfahren, und die Oeffentlichkeit wuesste heute noch kein Wort ueber den Fall. Nun passierte die Sache aber leider Herrn Torres nicht zu Hause, sondern im Theater. Die Stuhlreihen waren schmal. Und nun interessiert sich eine ganze Stadt fuer die Angelegenheit. Herr Torres hat naemlich die Rechnung, die der Arzt sandte, keineswegs bezahlt, sondern dem Dramatiker geschickt, der an dem Malheur die Schuld traegt. Denn - argumentiert der Rechtsanwalt des Verletzten - waere das Theaterstueck amuesanter gewesen, waere Herr Torres nicht eingeschlafen. Waere er nicht eingeschlafen, waere er nicht vom Stuehlchen gefallen. Waere er nicht vom Stuehlchen gefallen, haette er sich nicht wehgetan. Also: er verletzte sich, weil das Stueck schlecht war. Der Fall liegt eigentlich klar. Aber nur fuer Herrn Torres und seinen Rechtsanwalt. Der Stueckeschreiber und dessen Rechtsanwalt sind natuerlich ganz anderer Ansicht. Da waehrend der Auffuehrung - wenn sie auch schlecht war - von zweitausend Besuchern nur ein einziger vom Stuhl fiel, scheint dieser Fall doch wohl mehr auf Kosten dieses Besuchers, als auf die des Stueckes gesetzt werden zu duerfen. Immerhin ist der Prozess noch im Gange. Wir wollen hoffen, dass Herr Torres mit seiner Klage abgewiesen wird. Denn wo kaemen wir hin, wenn es den Autoren so erschwert wuerde, langweilige Dramen auffuehren zu lassen?";
-    int zeichnen = 0;
-    int i = 0;
-    int e = 0, z = 0, A = 0, M = 0;
-    int buchstabe = 0, leerzichnen = 0;
-    int Quersumme = 0;
-    int funkQuersumme = 0;
-    char strKlein[] = "";
-    char funkStrKlein[] = "";
-    //----------------------------------------------------------------------------------------------------------------
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        zeichnen++;
-        switch (str[i])
-        {
 
-        case ('e'):
-            e++;
-            break;
-        case ('z'):
-            z++;
-            break;
-        case ('A'):
-            A++;
-            break;
-        case ('M'):
-            M++;
-            break;
-        }
-    }
-    //---------------------------------------------------------------------------------------------------------
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        int z = str[i];
-        if (z >= 'A' && z <= 'Z' || z >= 'a' && z <= 'z')
-        {
-            buchstabe++;
-        }
-        if (z == 32)
-        {
-            leerzichnen++;
-        }
-        //-------------------------------------------------------------------------------------------------------------
-        Quersumme += str[i];
-        funkQuersumme = funkQuersumme + ascii(str[i]);
-        //-------------------------------------------------------------------------------------------------------------
-        strKlein[i] = str[i];
-        if (strKlein[i] > 'A' && strKlein[i] < 'Z')
-        {
-            strKlein[i] = strKlein[i] + 32;
-        }
-        //----------------------------------------------------------------------------------------------
-        // funkStrKlein[i](str[i]);
-    }
-    //----------------------------------------------------------------------------------------------------------
-    printf("%d Zeichen enthält in der Text\n", zeichnen);
-    printf("%d e enthält in der Text\n", e);
-    printf("%d z enthält in der Text\n", z);
-    printf("%d A enthält in der Text\n", A);
-    printf("%d M enthält in der Text\n", M);
-    printf("%d e enthält in der Text\n", funk(str, 'e'));
-    printf("%d z enthält in der Text\n", funk(str, 'z'));
-    printf("%d A enthält in der Text\n", funk(str, 'A'));
-    printf("%d M enthält in der Text\n", funk(str, 'M'));
-    printf("%d buchstabe enthält in der Text\n", buchstabe);
-    printf("%d leerzichnen enthält in der Text\n", leerzichnen);
-    printf("%d\n", funk1('.'));
-    printf("%d ist die Quersumme\n", Quersumme);
-    printf("%d ist die Quersumme mit funktion\n", funkQuersumme);
-    printf("%s\n", strKlein);
     return 0;
 }
